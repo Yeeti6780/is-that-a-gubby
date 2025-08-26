9b6bc8c0fcf517fd09b8bc2b101ad3308ecf0f43
raleigh <58990470+raleighed@users.noreply.github.com>
2025-08-26 01:01:32 +0100

    people who use "p:" to try getting a response from the bot

--- src/commands/inside_joke/.js
+++ src/commands/inside_joke/.js
@@ -1,7 +1,28 @@
 module.exports = {
     name: [''],
     args: [],
-    execute: async function (msg) {
+    execute: async function (msg, args) {
+        let poopy = this
+        let { chat, randomChoice, fetchPingPerms } = poopy.functions
+        let arrays = poopy.arrays
+        
+        if (process.env.AI21_KEY && args.length > 1) {
+            var saidMessage = args.slice(1).join(' ')
+
+            var chatResponse = await chat(saidMessage, msg, {
+                errorMsg: randomChoice(arrays.eightball)
+            }).catch(() => { }) ?? "what"
+            
+            if (!msg.nosend) await msg.reply({
+                content: chatResponse,
+                allowedMentions: {
+                    parse: fetchPingPerms(msg)
+                }
+            }).catch(() => { })
+            
+            return chatResponse
+        }
+        
         if (!msg.nosend) await msg.reply("crimes").catch(() => { })
         return "crimes"
     },
