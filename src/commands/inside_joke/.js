154c44672be7b5a540571119246e079250ac0cd8
raleigh <58990470+raleighed@users.noreply.github.com>
2026-01-18 03:25:03 +0000

    fix

--- src/commands/inside_joke/.js
+++ src/commands/inside_joke/.js
@@ -4,11 +4,25 @@ module.exports = {
     execute: async function (msg, args) {
         let poopy = this
         let { chat, randomChoice, fetchPingPerms } = poopy.functions
+        let { DiscordTypes } = poopy.modules
         let arrays = poopy.arrays
-        
-        if (process.env.AI21_KEY && args.length > 1) {
-            var saidMessage = args.slice(1).join(' ')
+        let config = poopy.config
 
+        var bypassPerms = (
+            msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.ManageGuild) ||
+            msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.ManageMessages) ||
+            msg.member.permissions.has(DiscordTypes.PermissionFlagsBits.Administrator) ||
+            msg.author.id === msg.guild.ownerId ||
+            (config.ownerids.find(id => id == msg.author.id))
+        )
+        
+        if (
+            process.env.AI21_KEY &&
+            !(data.guildData[msg.guild.id]?.disabled.find(cmd => cmd.find(n => n === "chat")) && !bypassPerms) &&
+            args.length > 1
+        ) {
+            var saidMessage = args.slice(1).join(' ')
+            
             var chatResponse = await chat(saidMessage, msg, {
                 errorMsg: randomChoice(arrays.eightball)
             }).catch(() => { }) ?? "what"
