a4126055dd69d663022beba4e6dc8ef88295a217
raleigh <58990470+raleighed@users.noreply.github.com>
2026-02-21 15:29:38 +0000

    splice

--- src/commands/unique/messages.js
+++ src/commands/unique/messages.js
@@ -243,7 +243,6 @@ module.exports = {
                         content: CryptoJS.AES.encrypt(cleanMessage, process.env.AUTH_TOKEN).toString(),
                         timestamp: Number.MAX_SAFE_INTEGER // genius
                     })
-                    data.guildData[msg.guild.id].messages.splice(50000)
 
                     tempdata[msg.guild.id].messages.unshift({
                         id: null,
@@ -251,7 +250,6 @@ module.exports = {
                         content: cleanMessage,
                         timestamp: Number.MAX_SAFE_INTEGER // genius
                     })
-                    tempdata[msg.guild.id].messages.splice(50000)
 
                     if (!msg.nosend) await msg.reply({
                         content: `✅ Added ${cleanMessage}`,
