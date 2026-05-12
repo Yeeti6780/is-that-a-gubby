095f010eb3dca26ff5f943918e452e9872c05c2e
raleigh <58990470+raleighed@users.noreply.github.com>
2026-05-12 02:20:37 +0100

    globale

--- src/special/functions/tbbstageorder.js
+++ src/special/functions/tbbstageorder.js
@@ -1,16 +1,24 @@
 module.exports = {
-    helpf: '(stage)',
-    desc: 'Returns the order of the TBB stage according to its chapter.',
+    helpf: '(stage | global)',
+    desc: 'Returns the order of the TBB stage according to its chapter or globally.',
     func: function (matches) {
         let poopy = this
         let json = poopy.json
+        let { splitKeyFunc } = poopy.functions
 
-        var stage = matches[1]
+        var [stage, global] = splitKeyFunc(matches[1], { args: 2 })
 
-        var chapters = Object.entries({ ...json.stageJSON.main, ...json.stageJSON.sub })
+        var chapters = Object.values({ ...json.stageJSON.main, ...json.stageJSON.sub })
+        if (global) chapters = [[chapters.flat()]]
 
-        var findChapter = chapters.find(([_, stages]) => stages.some(s => s.name.toLowerCase() == stage.toLowerCase().trim()))
+        var findChapter = chapters.find(
+            (stages) => stages.some(
+                s => s.name.toLowerCase().includes(stage.toLowerCase().trim())
+            )
+        )
 
-        return findChapter ? findChapter[1].findIndex(s => s.name.toLowerCase() == stage.toLowerCase().trim()) + 1 : ""
+        return findChapter ? findChapter.findIndex(
+            s => s.name.toLowerCase() == stage.toLowerCase().trim()
+        ) + 1 : ""
     }
 }
